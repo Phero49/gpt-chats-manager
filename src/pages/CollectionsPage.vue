dc5fe5715f32ccb2498c5237da4f37cf8d5111a9
Phero49 <pheromkuka49@gmail.com>
2023-04-23 06:45:10 +0200

    collections page

---
+++ src/pages/CollectionsPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page padding>
+    <div>
+      <CollectionComp :prewiew="false" />
+    </div>
+
+    <q-page-sticky position="bottom-right" :offset="[18, 18]">
+      <q-btn color="primary" fab icon="add" @click="onClick">
+        <q-tooltip> add new collection </q-tooltip>
+      </q-btn>
+    </q-page-sticky>
+  </q-page>
+</template>
+
+<script setup>
+import CollectionComp from "src/components/CollectionComp.vue";
+</script>
