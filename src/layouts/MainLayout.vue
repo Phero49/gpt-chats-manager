e48ae137b80ae9fa727bbbe1044c31e1a34ff436
Phero49 <pheromkuka49@gmail.com>
2023-08-01 10:18:38 +0200

    merge  new and old chats

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,57 +1,66 @@
 <template>
-  <q-layout view="lHh LpR fff">
-    <q-header
-      v-if="$route.name != 'editChat'"
-      :elevated="false"
-      bordered
-      class="bg-grey-8"
-    >
+  <q-layout view="lHh LpR lff">
+    <q-header v-if="$route.name != 'editChat'" elevated class="bg-white">
       <q-toolbar>
-        <q-toolbar-title
-          ><q-icon name="manager" /> Chats Manager
+        <q-btn
+          color="black"
+          flat
+          icon="menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+          v-if="!leftDrawerOpen"
+        />
+        <q-toolbar-title class="text-black"
+          ><q-icon name="work_history" /> Chats Manager
         </q-toolbar-title>
 
         <div>
           <q-btn
-            icon="folder"
+            icon="chat_bubble"
             to="/"
-            :color="$route.path == '/' ? 'green' : 'white'"
+            :color="$route.path == '/' ? 'green' : 'black'"
             label="my chats "
             flat
           />
         </div>
 
         <div>
-          <q-btn icon="backup" label="backup" flat>
-            <q-menu>
-              <q-list>
-                <q-item clickable v-ripple @click="authi">
-                  <q-item-section avatar>
-                    <q-avatar>
-                      <img
-                        src="src/assets/Google_Drive_icon_(2020).svg"
-                        alt=""
-                      />
-                    </q-avatar>
-                  </q-item-section>
-                  <q-item-section>Google drive</q-item-section>
-                </q-item>
-              </q-list>
-            </q-menu>
+          <q-btn
+            color="black"
+            label="export database"
+            dense
+            flat
+            @click="importdb"
+          >
+          </q-btn>
+        </div>
+        <div>
+          <q-btn
+            color="black"
+            dense
+            label="import database"
+            flat
+            @click="exportdb"
+          >
           </q-btn>
         </div>
 
         <div>
           <q-btn
-            :color="$route.path == '/help' ? 'green' : 'white'"
-            icon="help"
-            label="help"
+            color="red"
             flat
-            to="/help"
+            dense
+            label="delete dabase "
+            @click="confirmDelete = true"
           />
         </div>
+
         <div>
-          <q-btn flat @click="onClick" label="about">
+          <q-btn
+            flat
+            @click="onClick"
+            label="about"
+            :color="$route.path == '/help' ? 'green' : 'black'"
+          >
             <q-tooltip> learn about this app</q-tooltip>
           </q-btn>
         </div>
@@ -59,12 +68,30 @@
     </q-header>
     <q-footer
       v-if="$route.path == '/'"
-      elevated
+      :elevated="false"
       :height-hint="20"
       class="bg-black transparent"
     >
       <div class="q-py-sm">
-        <div></div>
+        <div>
+          <q-card-actions align="center">
+            <q-btn color="blue-6" round icon="bi-twitter" flat @click="onClick">
+              <q-tooltip> follow me on twitter </q-tooltip>
+            </q-btn>
+            <q-btn color="blue-8" round icon="bi-linkedin" flat>
+              <q-tooltip> follow me on linked </q-tooltip>
+            </q-btn>
+            <q-btn
+              color="primary"
+              round
+              icon="bi-facebook"
+              flat
+              @click="onClick"
+            >
+              <q-tooltip> follow me on facebook </q-tooltip>
+            </q-btn>
+          </q-card-actions>
+        </div>
         <div
           class="text-center q-py-sm text-subtitle1 text-capitalize text-black"
         >
@@ -78,229 +105,214 @@
       bordered
       v-model="leftDrawerOpen"
       show-if-above
+      :class="$route.path === '/' ? 'bg-grey-10' : ''"
     >
-      <q-list v-mutation="handler" v-if="$route.name == 'editChat'">
-        <div class="text-body1 text-weight-medium text-center q-my-md">
-          Chat outline
-        </div>
-        <div
-          id="container"
-          @click="
-            (e) => {
-              e.preventDefault();
-            }
-          "
-        >
-          <div
-            :draggable="true"
-            v-for="(item, index) in store.contents"
-            :key="index"
-            dropzone="move"
-            aria-dropeffect="popup"
-            @dragover="
-              (e) => {
-                e.preventDefault();
-              }
-            "
-            @dragstart="onDragEnter(index, $event)"
-            @drop="onDrop(index, $event)"
-            :id="`item-${index}`"
-          >
-            <q-item @click="scrollTo(index)" clickable>
-              <q-item-section top avatar>
-                <q-avatar color="black" size="sm" text-color="white">
-                  {{ index + 1 }}
-                </q-avatar>
-              </q-item-section>
-              <q-item-section>
-                <q-item-label
-                  lines="2"
-                  class="text-subtitle1 headings"
-                  caption
-                  >{{ item }}</q-item-label
-                >
-              </q-item-section>
-              <q-item-section side v-if="selectedItem == index">
-                <q-card-actions>
-                  <q-btn
-                    round
-                    dense
-                    size="sm"
-                    flat
-                    icon="delete"
-                    @click="clickHandler1"
-                  />
-                </q-card-actions>
-              </q-item-section>
-            </q-item>
-          </div>
-        </div>
-      </q-list>
+      <chat-outline v-if="$route.name == 'editChat'" />
       <!--recent chats-->
-      <q-scroll-area
-        style="overflow-x: hidden"
-        class="fit"
-        :thumb-style="{
-          right: '2px',
-          borderRadius: '5px',
-          backgroundColor: 'white',
-          width: '5px',
-          opacity: '0.75',
-        }"
-      >
-        <q-list
-          v-if="$route.path === '/'"
-          separator
-          class="bg-grey-10 text-white q-px-sm"
-        >
-          <div class="text-body1 q-py-md">
-            <q-icon name="recent" /> Recently opened chats
-          </div>
 
-          <q-item
-            v-for="(key, i) in Object.keys(recentStore.recent)"
-            :to="`/exportDocs?url=${key}`"
-            :key="i"
-          >
-            <q-item-label lines="1" class="text-white" caption>
-              {{ cheerio.load(recentStore.recent[key]).text() }}
-            </q-item-label>
-          </q-item>
-        </q-list>
-      </q-scroll-area>
+      <q-list v-if="$route.path === '/'" separator class="text-white q-px-sm">
+        <div
+          class="text-body1 q-px-sm q-py-sm q-mb-sm row justify-between shadow-1 items-center bg-black"
+          style="border-radius: 6px"
+        >
+          <div>Recently opened chats</div>
+          <div>
+            <q-btn
+              color="white"
+              icon="close"
+              round
+              flat=""
+              @click="leftDrawerOpen = false"
+            />
+          </div>
+        </div>
+
+        <q-item
+          v-for="(key, i) in Object.keys(recentStore.recent)"
+          :to="`/exportDocs?url=${key}`"
+          :key="i"
+        >
+          <q-item-label lines="1" class="text-white" caption>
+            {{ cheerio.load(recentStore.recent[key]).text() }}
+          </q-item-label>
+        </q-item>
+      </q-list>
     </q-drawer>
 
     <q-page-container>
       <router-view />
     </q-page-container>
+
+    <q-dialog v-model="confirmDelete" persistent>
+      <q-card>
+        <q-card-section class="row items-center">
+          <q-avatar icon="warning" color="red" text-color="white" />
+          <span class="q-ml-sm"
+            >Your are about to delete all data on this extension are you sure
+          </span>
+        </q-card-section>
+        <q-card-actions align="right">
+          <q-btn flat label="Cancel" color="primary" v-close-popup />
+          <q-btn flat label="Yes" color="red" @click="deletedb" />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
   </q-layout>
 </template>
 
 <script setup>
-import { defineComponent, ref, onMounted, onUnmounted } from "vue";
+import { ref, onUnmounted } from "vue";
 import * as cheerio from "cheerio";
 
 import { useQuasar } from "quasar";
 import { useRouter } from "vue-router";
-import { makeEven } from "src/js/getIds";
 import { docStore } from "../stores/curentDocStore";
 import { recent } from "src/stores/recent";
-import DriveIcon from "src/assets/DriveIcon.vue";
-import {
-  getAuth,
-  signInWithPopup,
-  GoogleAuthProvider,
-  signInWithRedirect,
-  onAuthStateChanged,
-} from "firebase/auth";
 
-const auth = getAuth();
-const apiKey = auth.app.options.apiKey;
+import ChatOutline from "src/components/chatOutline.vue";
 
-const authi = async () => {
-  const provider = new GoogleAuthProvider();
-  await signInWithPopup(auth, provider);
+//TODO:import not yet implemented
 
-  $q.notify("Text has been highlighted");
-};
-const clientID =
-  "468883781787-l5l5p17fsl57h0c9b129c3ju3ldmsfol.apps.googleusercontent.com";
-const scope =
-  "https://www.googleapis.com/auth/drive.appdata,https://www.googleapis.com/auth/drive.appfolder";
-async function SignupWithGoogle() {
-  const provider = new GoogleAuthProvider();
-  try {
-    const user = await signInWithPopup(auth, provider);
-    const { accessToken } = user.user;
+async function exportdb() {
+  //this function exports  the whole db as a json
+  const { data } = await $q.bex.send("exportdb");
+  const a = document.createElement("a");
+  const dataURI =
+    "data:application/json;charset=utf-8," + encodeURIComponent(data);
 
-    if (user != null) {
-      //   console.log(user);
-      try {
-        const response = await api.get("/login", {
-          headers: { authorization: `Bearer ${accessToken}` },
-        });
-        const customToken = response.data;
-
-        setPersistence(auth, browserLocalPersistence)
-          .then(() => {
-            return signInWithCustomToken(auth, customToken);
-          })
-          .then((u) => {
-            console.log(u);
-            console.log(auth.currentUser);
-          });
-      } catch (error) {
-        console.log(error);
-      }
-    }
-  } catch (error) {
-    console.log(error);
-  }
-}
-function backup(params) {
-  onAuthStateChanged(auth, (user) => {
-    if (user != null) {
-    } else {
-      SignupWithGoogle();
-    }
+  a.href = dataURI;
+  a.download = "chats-manager.json";
+  a.click();
+  $q.notify({
+    message: "data succefully exported",
+    color: "green",
+    badgeColor: "green",
+    position: "bottom-left",
   });
+}
+
+const confirmDelete = ref(false);
+
+async function deletedb() {
+  await $q.bex.send("deletedb");
+  confirmDelete.value = false;
+  router.push("/");
 }
 
 const recentStore = recent();
 const store = docStore();
 const router = useRouter();
 const $q = useQuasar();
-function today() {
-  let dayDate = new Date().toLocaleDateString("YYYYMMDD");
-  let dateArray = dayDate.split("/");
-
-  let fixedDate = dateArray.map((item) => {
-    if (item.length < 2) {
-      return `0${item}`;
-    } else {
-      return item;
-    }
-  });
-
-  let mothDay = fixedDate.splice(0, 2);
-  console.log(fixedDate.concat(mothDay).join("/"));
-  return fixedDate.concat(mothDay).join("/");
-}
-
-const date = ref(today());
 
 const chatContents = store.contents;
-const selectedItem = ref(0);
+const saveChat = async (chatStructure) => {
+  //save for the first time
+  const chatContents = chatStructure;
+  await $q.bex.send("saveChats", {
+    key: chatContents.url,
+    content: chatContents,
+  });
+  router.push(`/exportDocs?url=${chatStructure.url}`);
+};
+$q.bex.once("get_chat", ({ data }) => {
+  var chatStructure = {
+    contents: [],
+    htmlString: "",
+    title: "",
+    date: null,
+    url: "",
+    numberOfQuestions: 0,
+  };
 
-function scrollTo(index) {
-  selectedItem.value = index;
-  const element = document.querySelector(`#group-${makeEven(index)}`);
-  element.scrollIntoView({ behavior: "smooth", block: "start" });
-  element.classList.add("bg-yellow-4");
+  if (data != null) {
+    if (data.chat) {
+      // If 'chat' property exists in the 'data' object
+      const newChatData = data;
+      chatContents.value = data.askedQuestions;
 
-  setTimeout(() => {
-    element.classList.remove("bg-yellow-4");
-  }, 3000);
-}
+      const title = `${data.title}`;
+      store.$reset();
 
-$q.bex.once("get_chat", (data) => {
-  if (data.data.chat) {
-    const mydata = data.data;
-    chatContents.value = data.data.req;
+      setTimeout(async () => {
+        // Set the 'url' and 'title' in the 'store' object
+        chatStructure.url = newChatData.chat_url;
+        chatStructure.title = title;
 
-    const title = `<h6>${data.data.title}</h6>`;
-    store.$reset();
-    setTimeout(() => {
-      store.contents = mydata.req;
+        // Fetch chat data using the 'getSingleChat' endpoint
 
-      store.url = mydata.chat_url;
-      store.title = title;
-      store.htmlString = mydata.chat.join("");
+        const { data: chatData } = await $q.bex.send("getSingleChat", {
+          key: chatStructure.url,
+        });
 
-      router.push("/exportDocs");
-    }, 400);
+        const oldChatData = chatData;
+        console.log("old", oldChatData);
+        if (oldChatData === null) {
+          var date = new Date().toLocaleDateString();
+          date = new Date(date).getTime();
+          chatStructure.date = date;
+          // If no previous chat data exists, update 'store.contents' and 'store.htmlString'
+          chatStructure.contents = newChatData.askedQuestions;
+          chatStructure.htmlString = newChatData.chat.join("");
+          chatStructure.numberOfQuestions = chatStructure.contents.length;
+          chatStructure.date = date;
+          await saveChat(chatStructure);
+        } else {
+          chatStructure = oldChatData;
+          // If previous chat data exists, handle the updates
+          var { numberOfQuestions } = chatStructure;
 
-    console.log(data, "mydata");
+          const newQuestionNumber = newChatData.askedQuestions.length;
+
+          console.log(newQuestionNumber, numberOfQuestions);
+
+          const questionNumberDiffernce = newQuestionNumber - numberOfQuestions;
+
+          if (questionNumberDiffernce > 0) {
+            // Calculate the difference in the number of questions
+            //const difference = newQuestionNumber - numberOfQuestions;
+
+            // Extract the new questions and additional chats to append
+
+            numberOfQuestions -= 1;
+            const newContent =
+              newChatData.askedQuestions.splice(numberOfQuestions);
+
+            const additionalChats = newChatData.chat.splice(numberOfQuestions);
+
+            //  console.log("additional chats", additionalChats);
+
+            // Concatenate newContent with contents and assign it back to contents
+            console.log(newContent);
+
+            chatStructure.contents = chatStructure.contents.concat(newContent);
+            // Append additionalChats to htmlString
+            console.log(oldChatData, "new chat", newChatData);
+
+            const additionalChatsString = additionalChats.join("");
+
+            chatStructure.htmlString =
+              chatStructure.htmlString + additionalChatsString;
+            chatStructure.numberOfQuestions += questionNumberDiffernce;
+            console.log(chatStructure);
+            await saveChat(chatStructure);
+          } else {
+            router.push(`/exportDocs?url=${chatStructure.url}`);
+          }
+        }
+
+        // Redirect to "/exportDocs" after the timeout
+      }, 1);
+    }
+  } else {
+    // If 'chat' property does not exist in 'data', show an error dialog
+    $q.dialog({
+      message: `Error: Something went wrong. 
+      Make sure your chat link ends with something 
+      like c/5551925e-61ba-45f2-b5da-10258fc664de and
+       not something like model=text-davinci-002-render-sha . 
+       if it ends with model=text-davinci-002-render-sha refresh your chat page `,
+      dark: true,
+    });
   }
 });
 
@@ -309,71 +321,8 @@ onUnmounted(() => {
 });
 
 const leftDrawerOpen = ref(false);
-function handler() {}
-leftDrawerOpen,
-  function toggleLeftDrawer() {
-    leftDrawerOpen.value = !leftDrawerOpen.value;
-  };
 
-const dr = ref();
-const draggedItemIndex = ref();
-function onDragEnter(index, e) {
-  dr.value = e.target;
-  draggedItemIndex.value = index;
-
-  console.log(e);
-}
-
-const exportchat = async () => {
-  const { data, respond } = await $q.bex.send("exportChat", {});
-};
-function onDrop(index, e) {
-  // Get the dragged item and remove it from the contents array
-  let draggedItem = store.contents.splice(draggedItemIndex.value, 1)[0];
-
-  // Insert the dragged item at the new index
-  store.contents.splice(index, 0, draggedItem);
-
-  console.log(index + 1, "drop", draggedItemIndex.value + 1);
-
-  // Get the parent node and its children
-  const parentNode = document.querySelector("#editorContent");
-  const children = Array.from(parentNode.children);
-
-  console.log(children);
-
-  // If there is more than one child element
-  if (children.length > 1) {
-    // Get the element to replace
-
-    const draggedGroup = children[draggedItemIndex.value];
-    const id1 = draggedGroup.getAttribute("id");
-
-    // Get the element to replace with
-    const replaceOn = children[index];
-    const dropedId = replaceOn.getAttribute("id");
-    const dropedIdInt = parseInt(dropedId.split("-")[1]);
-    console.log(dropedId, id1);
-
-    // Replace the elements by inserting them before/after each other
-
-    const insertedNode = parentNode.insertBefore(draggedGroup, replaceOn);
-
-    scrollTo(draggedItemIndex.value);
-    setTimeout(() => {
-      for (let i = 0; i < children.length; i++) {
-        const element = insertedNode.parentElement.children[i];
-        element.id = `group-${makeEven(i)}`;
-        console.log(element, element.id);
-      }
-    }, 1000);
-    //console.log(insertedNode, replaceOn, index);
-    // parentNode.insertBefore(draggedGroup, insertedNode);
-    // parentNode.insertBefore(draggedGroup, cc);
-
-    //  console.log(draggedGroup, draggedResponse);
-  }
-}
+leftDrawerOpen;
 </script>
 <style>
 .headings::first-letter {
